7dcc0c87a6b972913556bd84ad8db9958717e463
Gustavo Eyros <gustavoeyros@hotmail.com>
2022-12-12 10:45:19 -0300

    arithmetic operators

---
+++ js/operators.js
@@ -0,0 +1,20 @@
+// Arithmetic Operators
+
+let numberOne = 15
+let numberTwo = 10
+
+/* console.log(numberOne + numberTwo)
+console.log(numberOne - numberTwo)
+console.log(numberOne * numberTwo)
+console.log(numberOne / numberTwo)
+console.log(numberOne % numberTwo)
+console.log(numberOne ** numberTwo) */
+
+
+//Increment
+console.log(numberOne++)
+console.log(numberOne)
+
+
+//Decrement
+console.log(--numberOne)
