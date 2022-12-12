665b0f45f95cf2d6f9afa09041eca1a0b4105833
Gustavo Eyros <gustavoeyros@hotmail.com>
2022-12-12 11:46:27 -0300

    operators

--- js/operators.js
+++ js/operators.js
@@ -1,14 +1,14 @@
-// Arithmetic Operators
+// -------- Arithmetic Operators ---------------
 
-let numberOne = 15
+/* let numberOne = 15
 let numberTwo = 10
 
-/* console.log(numberOne + numberTwo)
+console.log(numberOne + numberTwo)
 console.log(numberOne - numberTwo)
 console.log(numberOne * numberTwo)
 console.log(numberOne / numberTwo)
 console.log(numberOne % numberTwo)
-console.log(numberOne ** numberTwo) */
+console.log(numberOne ** numberTwo) 
 
 
 //Increment
@@ -18,3 +18,131 @@ console.log(numberOne)
 
 //Decrement
 console.log(--numberOne)
+
+
+ */
+
+// --------- Assignment Operators ---------------
+
+
+/* let x = 15;
+
+x = x + 5; // equal to 
+x += 5;
+
+ */
+
+
+
+// --------------- Comparison Operators --------------
+
+/* let x = 5;
+
+//Relational
+console.log(x > 4);
+console.log(x >= 5);
+console.log(x < 5);
+console.log(x <= 5);
+
+//Equality
+console.log(x===5)
+console.log(x!==5)
+
+ */
+
+//  -------------- Equality Operators ------------------
+
+
+
+/* 
+// Strict Equality (Type + Value)
+console.log(5===5);
+console.log('5'===5);
+
+// Lose Equality (Value)
+console.log(5 == 5);
+console.log('5'== 5);
+
+  */
+
+
+// ---------- Ternary Operators -------------
+
+
+/* 
+// If a player has more than 100 achievements, he is a professional
+// If not, he will be amateur.
+
+let achievements = 200;
+let level = achievements > 100 ? "Professional" : "Amateur";
+
+console.log(level);
+
+ */
+
+
+//  --------------- Logical Operators with Non-booleans ----------------- 
+
+
+/* 
+// Logical AND (&&)
+// Return true if both conditions are true
+
+
+ let goodGrades = true;
+let dedicatedStudent = true;
+let result = goodGrades && dedicatedStudent;
+console.log(result)  
+ */
+
+
+/* // Logical OR (||)
+// Return true if one conditions are true
+
+let goodGrades = false;
+let dedicatedStudent = true;
+let result = goodGrades || dedicatedStudent;
+console.log(result) 
+  */
+
+
+
+/* // NOT (!)
+// Alter value to opposite
+
+ let dedicatedStudent = true;
+let advice = !dedicatedStudent;
+console.log(advice) 
+ */
+
+
+
+// ----------- BitWise Operators ----------
+
+/* console.log(1 | 2); //BitWise OR
+console.log(1 & 2); //BitWiser AND */
+
+
+// --------- Operator Precedence ----------
+
+/* let x = (2 + 8) * 10;
+console.log(x) */
+
+
+
+
+// ------------- Exercise --------------------
+
+/* let a = 'red'
+let b = 'blue'
+
+
+let c = a;
+a = b;
+b = c;
+
+
+console.log(a)
+console.log(b) */
+
+
